b5c014b01d2aeeedcee09e10b9b8f9399e1e9d51
shaimarri <shaikrupareddy@gmail.com>
2022-05-10 09:55:21 -0400

    updated files with latest changes

---
+++ models/staging/vw_worldcat_xwc_CDFRec_v600_s0.sql
@@ -0,0 +1,24 @@
+
+    {{ config(
+    schema='raw', 
+    materialized='view'
+        ) }}
+
+     -- depends_on: {{ ref('stg_worldcat_xwc') }}
+
+    with array_table as (
+
+   {{ json_array_to_table(
+        
+        model_name = 'raw.stg_worldcat_xwc', 
+        foreign_key = 'rowkey',
+        json_column = 'document_json', 
+        array_path = 'CDFRec.v600.s0'
+
+    )}}
+
+    )
+
+    select *
+    from array_table
+    
